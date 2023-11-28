4aa771d16ead410ecf7cb7d1e0950c3740e0d983
Jhordan3010 <jmhueran@utn.edu.ec>
2023-11-28 18:50:09 -0500

    interfaces

--- ClassDiagram/src/com/Docente.java
+++ ClassDiagram/src/com/Docente.java
@@ -12,6 +12,30 @@ public class Docente extends Persona {
         this.titulo = titulo;
         this.horasTrab = horasTrab;
     }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getHorasTrab() {
+        return horasTrab;
+    }
+
+    public void setHorasTrab(int horasTrab) {
+        this.horasTrab = horasTrab;
+    }
     
     
 }
--- ClassDiagram/src/com/Estudiante.java
+++ ClassDiagram/src/com/Estudiante.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com;
+public class Estudiante extends Persona{
+    private String codMateria;
+    private String carrera;
+    private int nivelCarrera;
+    private int nota1;
+    private int nota2;
 
-/**
- *
- * @author jhord
- */
-public class Estudiante {
+    public Estudiante(String codMateria, String carrera, int nivelCarrera, int nota1, int nota2, String dni, String nombre, boolean genero, Fecha fechaNac) {
+        super(dni, nombre, genero, fechaNac);
+        this.codMateria = codMateria;
+        this.carrera = carrera;
+        this.nivelCarrera = nivelCarrera;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public String getCodMateria() {
+        return codMateria;
+    }
+
+    public void setCodMateria(String codMateria) {
+        this.codMateria = codMateria;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public int getNivelCarrera() {
+        return nivelCarrera;
+    }
+
+    public void setNivelCarrera(int nivelCarrera) {
+        this.nivelCarrera = nivelCarrera;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
     
+     
 }
